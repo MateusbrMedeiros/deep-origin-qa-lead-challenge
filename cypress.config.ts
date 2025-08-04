aeecec6d35dd70fd62605f61f82f4ceba241cb9d
MateusbrMedeiros <mateusbrmedeiros@gmail.com>
2025-08-04 13:57:18 -0300

    feat: add github actions pipeline with Mochawesome reports

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'https://dummyjson.com',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here se precisar
+    },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      reporterEnabled: 'mochawesome',
+      mochawesomeReporterOptions: {
+        reportDir: 'cypress/reports',
+        overwrite: false,
+        html: true,
+        json: true,
+      },
     },
   },
 });
